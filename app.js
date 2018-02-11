ba305c6491b0f644687c0ee4e958c20771c6f4a3
melodylaniella <anitalesiak1226@interia.eu>
2018-02-11 11:38:47 +0100

    Uzupełnienie app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//require jest mapowane na endpointy a je¿eli nie to po prostu odpali w¹tek i //wykonuje to co w nim znajdzie czyli t¹ ostatni¹ linijkê ReceiveMessages()
+
 require("./modules/receive_message").lab;
 var urlMap = [];
 var service = require("./lib/service").http(urlMap);
